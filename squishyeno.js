b59c2fa7dcf79468b7339664c51456f40ab88140
XDleader555 <XDleader555@gmail.com>
2014-04-20 14:18:32 -0700

    Fixed dial caliper rotation
    Dial calipers rotate counter clockwise when closing

--- squishyeno.js
+++ squishyeno.js
@@ -823,7 +823,7 @@ var oef = function () {
    Caliper1.img.style.top = (pipe_center - 25 - pipe_gap/2)+'px';
    Caliper2.img.style.top = (pipe_center - 5 + pipe_gap/2)+'px';
    dialhand.img.style.top = (pipe_center + 25 + pipe_gap/2)+'px';
-   dialhand.img.style.webkitTransform = "rotate("+(-5*(pipe_center + pipe_gap/2)+180)+"deg)";
+   dialhand.img.style.webkitTransform = "rotate("+(5*(pipe_center + pipe_gap/2)+180)+"deg)";
 
    if (pipe_opening >= 0) {
     pipe_opening++;
